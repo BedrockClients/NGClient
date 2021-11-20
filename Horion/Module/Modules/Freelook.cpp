fa1ade62f1931cd746dd9cfa1e59db27292d3562
YDC-Deadly <88569435+YDC-Deadly@users.noreply.github.com>
2021-11-19 20:21:44 -0500

    Revert "Update Freelook.cpp"
    This reverts commit 04d1342c2a8c5db2d0f82993bc645fe4aa72fad9.

--- Horion/Module/Modules/Freelook.cpp
+++ Horion/Module/Modules/Freelook.cpp
@@ -17,6 +17,23 @@ void Freelook::onEnable() {
 }
 
 void Freelook::onDisable() {
+	if (g_Data.isInGame() && resetViewTick == -1 && redirectMouse) {
+		resetViewTick = isThirdPerson;
+		if (cameraFacesFront) {
+			lastCameraAngle.y += 180;
+			lastCameraAngle = lastCameraAngle.normAngles();
+			mouseEnd = lastCameraAngle;
+
+			initialViewAngles.y += 180;
+			initialViewAngles = initialViewAngles.normAngles();
+		} else {
+			mouseEnd = lastCameraAngle;
+			mouseEnd.y += 180;
+		}
+	} else if (!g_Data.isInGame()) {
+		redirectMouse = false;
+	}
+	deez = {};
 }
 
 void Freelook::onPostRender(C_MinecraftUIRenderContext* mode) {
@@ -26,4 +43,15 @@ void Freelook::onPostRender(C_MinecraftUIRenderContext* mode) {
 }
 
 void Freelook::onTick(C_GameMode* mode) {
+	if (resetViewTick == 0) {
+		redirectMouse = false;
+		resetViewTick = -1;
+		auto loc = g_Data.getLocalPlayer();
+		vec2_t l = initialViewAngles;
+		l = l.sub(mouseEnd).normAngles();
+		l.x = initialViewAngles.x - mouseEnd.x;  // dont norm this angle
+
+		l.x *= -1;
+		loc->applyTurnDelta(&l);
+	}
 }
