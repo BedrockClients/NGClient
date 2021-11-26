cea9c58dbcf945feb2499c38ea835d559f0057d6
NRG <jcazman19@yahoo.com>
2021-11-26 00:45:33 -0500

    Anticheat

--- SDK/CRakNetInstance.h
+++ SDK/CRakNetInstance.h
@@ -18,14 +18,14 @@ public:
 public:
 	virtual unsigned int getPort(void) const;
 
-private:
+public:
 	virtual __int64 getRefinedLocalIps(void) const;
 	virtual __int64 getConnectedGameInfo(void) const;
 
 public:
 	virtual void setupNatPunch(bool);
 
-private:
+public:
 	virtual __int64 getNatPunchInfo(void) const;
 	virtual __int64 startNatPunchingClient(__int64);
 	virtual __int64 addConnectionStateListener(__int64);
@@ -35,7 +35,7 @@ public:
 	virtual bool isIPv4Supported(void) const;
 	virtual bool isIPv6Supported(void) const;
 
-private:
+public:
 	virtual __int64 getIPv4Port(void) const;
 	virtual __int64 getIPv6Port(void) const;
 	virtual __int64 getGUID(void) const;
