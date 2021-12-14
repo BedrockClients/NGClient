f3ec0d06ae2d3cc833bf28c168672314f51f901b
NRG <jcazman19@yahoo.com>
2021-12-14 18:47:29 -0500

    Dqad

--- Memory/Hooks.cpp
+++ Memory/Hooks.cpp
@@ -677,7 +677,7 @@ __int64 Hooks::RenderText(__int64 a1, C_MinecraftUIRenderContext* renderCtx) {
 						constexpr float borderPadding = 1;
 						constexpr float margin = 5;
 
-						static std::string name = "Surge Client|NG";
+						static std::string name = "Surge Client";
 #ifdef _DEBUG
 						static std::string version = "";
 #elif defined _BETA
