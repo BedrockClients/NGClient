9f28db1aca2b0efb574b2df767baa8570e4412d1
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-20 15:17:05 -0500

    Made a module only use a .h
    Example

--- SDK/CMobEffect.h
+++ SDK/CMobEffect.h
@@ -4,9 +4,9 @@
 class CMobEffect {
 public:
 	virtual void Destructor();
-	virtual void applyEffects(C_Entity *, int, int);
-	virtual void removeEffects(C_Entity *);
-	virtual void applyInstantaneousEffect(C_Entity *, C_Entity *, C_Entity *, int, float);
+	virtual void applyEffects(C_Entity*, int, int);
+	virtual void removeEffects(C_Entity*);
+	virtual void applyInstantaneousEffect(C_Entity*, C_Entity*, C_Entity*, int, float);
 	virtual bool isInstantaneous(void);
 	virtual int getAttributeModifierValue(int, class AttributeModifier const &);
 };
