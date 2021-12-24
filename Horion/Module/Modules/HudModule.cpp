ac9975bad1a77591d37ea1866a76d1ab4b584b45
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2021-12-23 21:08:20 -0500

    Looks Like shit

--- Horion/Module/Modules/HudModule.cpp
+++ Horion/Module/Modules/HudModule.cpp
@@ -6,7 +6,7 @@ HudModule::HudModule() : IModule(0, Category::GUI, "Displays Hud") {
 	registerBoolSetting("HUD", &Hud, Hud);
 	registerBoolSetting("RGB", &rgb, rgb);
 	registerBoolSetting("MSG", &Msg, Msg);
-	registerBoolSetting("Show ArmorHUD", &displayArmor, displayArmor); //it does not render in a random spot I render on side of your screen
+	//registerBoolSetting("Show ArmorHUD", &displayArmor, displayArmor); //This looks like complete shit (respectully) so im removing it.
 	registerBoolSetting("ClickToggle", &clickToggle, clickToggle);
 	registerBoolSetting("Watermark", &watermark, watermark);
 	registerBoolSetting("Bools", &bools, bools);
