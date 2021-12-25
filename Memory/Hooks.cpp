ccd23232fb72856cb673b4ed64800e1951d788b1
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-25 14:12:28 -0500

    Fix C_InventoryTransactionPacket!

--- Memory/Hooks.cpp
+++ Memory/Hooks.cpp
@@ -1308,6 +1308,9 @@ void Hooks::LoopbackPacketSender_sendToServer(C_LoopbackPacketSender* a, C_Packe
 	static auto nofall = moduleMgr->getModule<NoFall>();
 	static auto noPacketMod = moduleMgr->getModule<NoPacket>();
 	static auto tp = moduleMgr->getModule<Teleport>();
+	//if (packet->isInstanceOf<C_InventoryTransactionPacket>()) Good for testing packet sigs
+	//	g_Data.getLocalPlayer()->jumpFromGround();
+
 	if (noPacketMod->isEnabled() && g_Data.isInGame())
 		return;
 
