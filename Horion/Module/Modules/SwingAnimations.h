3cfc6b43b8fdf379e44f46e4f5795b298445613d
NRG <jcazman19@yahoo.com>
2021-12-26 14:23:08 -0500

    Fixed

--- Horion/Module/Modules/SwingAnimations.h
+++ Horion/Module/Modules/SwingAnimations.h
@@ -1,26 +1,22 @@
 #pragma once
+#include "../ModuleManager.h"
 #include "Module.h"
 class SwingAnimations : public IModule {
-private:
-	void* eightbitddress;
-
-	void* floppyAddress;
-	void* floppyAddress2;
-
-	void* fluxAddress;
-
-	void* noObstructAddress;
-
-	void* pushAddress;
-	void* pushAddress2;
-
-	SettingEnum setting;
 public:
+	void* targetAddress;
+	SettingEnum enum1;
+
 	SwingAnimations();
 	~SwingAnimations();
 
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
 	virtual void onEnable() override;
+	virtual void onTick(C_GameMode* gm) override;
+	virtual void onPreRender(C_MinecraftUIRenderContext* renderCtx);
+	virtual void onPostRender(C_MinecraftUIRenderContext* renderCtx) override;
+	virtual void onSendPacket(C_Packet* p) override;
+	virtual void onMove(C_MoveInputHandler* hand) override;
 	virtual void onDisable() override;
+	void onLevelRender() override;
 };
