a6350bc6c1f8b4523a5bb5aa27b54d00e159240d
NRG <jcazman19@yahoo.com>
2021-12-28 15:00:09 -0500

    Best antivoid EVER

--- Horion/Module/Modules/AntiVoid.cpp
+++ Horion/Module/Modules/AntiVoid.cpp
@@ -35,22 +35,17 @@ void AntiVoid::onTick(C_GameMode* gm) {
 	}
 	if (player->fallDistance >= distance) {
 		if (mode.selected == 0) {
+			g_Data.getClientInstance()->getMoveTurnInput()->clearMovementState();
 			player->setPos(orgipos);
 			static auto hopMod = moduleMgr->getModule<Bhop>();
 			static auto speedMod = moduleMgr->getModule<Speed>();
-			static auto scaffMod = moduleMgr->getModule<Scaffold>();
-			static auto towerMod = moduleMgr->getModule<Tower>();
 			static auto surge = moduleMgr->getModule<ClickGuiMod>();
 			hopMod->setEnabled(false);
 			speedMod->setEnabled(false);
-			if (!scaffMod->isEnabled())
-				scaffMod->setEnabled(true);
-			if (!towerMod->isEnabled())
-				towerMod->setEnabled(true);
 			if (surge->surge)
-				g_Data.getGuiData()->displayClientMessageF("[%sAntiVoid%s] %sDisabled Speed, Bhop, and enabled Scaffold & Tower%s", BLUE, WHITE, BLUE, WHITE, configMgr, WHITE);
+				g_Data.getGuiData()->displayClientMessageF("[%sAntiVoid%s] %sDisabled Speed, Bhop, and disabled move keys%s", BLUE, WHITE, BLUE, WHITE, configMgr, WHITE);
 			else
-				g_Data.getGuiData()->displayClientMessageF("[%sAntiVoid%s] %sDisabled Speed, Bhop, and enabled Scaffold & Tower%s", LIGHT_PURPLE, WHITE, LIGHT_PURPLE, WHITE, configMgr, WHITE);
+				g_Data.getGuiData()->displayClientMessageF("[%sAntiVoid%s] %sDisabled Speed, Bhop, and disabled move keys%s", LIGHT_PURPLE, WHITE, LIGHT_PURPLE, WHITE, configMgr, WHITE);
 		}
 		if (mode.selected == 1) {
 			float dist = gm->player->getPos()->dist(orgipos);
