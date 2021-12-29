da13f551979835ab89e02008b1cb105d45c1c759
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-29 18:55:24 -0500

    .h's

--- Horion/Command/Commands/TeleportCommand.h
+++ Horion/Command/Commands/TeleportCommand.h
@@ -2,9 +2,24 @@
 #include "ICommand.h"
 class TeleportCommand : public IMCCommand {
 public:
-	TeleportCommand();
-	~TeleportCommand();
+	TeleportCommand() : IMCCommand("tp", "Teleports to coordinates", "<X> <Y> <Z>") {
+		registerAlias("teleport");
+		registerAlias("setpos");
+	}
+	~TeleportCommand(){};
 
 	// Inherited via IMCCommand
-	virtual bool execute(std::vector<std::string>* args) override;
+	virtual bool execute(std::vector<std::string>* args) override {
+		assertTrue(g_Data.getLocalPlayer() != nullptr);
+		assertTrue(args->size() >= 4);
+
+		vec3_t pos;
+		pos.x = assertFloat(args->at(1));
+		pos.y = assertFloat(args->at(2)) + 1;
+		pos.z = assertFloat(args->at(3));
+
+		g_Data.getLocalPlayer()->setPos(pos);
+		clientMessageF("%sTeleported!", GREEN);
+		return true;
+	}
 };
