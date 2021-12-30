9fd9267c5786504a33b7899eb94e3ea594263536
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-29 19:06:17 -0500

    .h owowow

--- Horion/Command/Commands/DamageCommand.h
+++ Horion/Command/Commands/DamageCommand.h
@@ -2,9 +2,14 @@
 #include "ICommand.h"
 class DamageCommand : public IMCCommand {
 public:
-	DamageCommand();
-	~DamageCommand();
+	DamageCommand() : IMCCommand("damage", "Damage yourself (relies on attacking yourself)", "<hi>") {
+		registerAlias("dmg");
+	}
+	~DamageCommand(){}
 
 	// Inherited via IMCCommand
-	virtual bool execute(std::vector<std::string>* args) override;
+	virtual bool execute(std::vector<std::string>* args) override {
+		g_Data.getCGameMode()->attack(g_Data.getLocalPlayer());
+		return true;
+	}
 };
