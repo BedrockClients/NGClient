fc0b0e0e312cc60b4de26849b3bfa81af7df4381
DeadtrosGaming <isaiahmlam200@outlook.com>
2021-12-29 20:16:49 -0500

    .h

--- Horion/Module/Modules/AutoJump.h
+++ Horion/Module/Modules/AutoJump.h
@@ -1,12 +1,21 @@
 #pragma once
+#include "../ModuleManager.h"
 #include "Module.h"
-
 class AutoJump : public IModule {
 public:
-	AutoJump();
-	~AutoJump();
 
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
-	virtual void onTick(C_GameMode* gm) override;
+	AutoJump() : IModule(0x0, Category::MOVEMENT, "Automatically jump"){
+
+	};
+	~AutoJump(){};
+
+	void onTick(C_GameMode* gm) {
+		auto player = g_Data.getLocalPlayer();
+
+		if (player->onGround) gm->player->jumpFromGround();
+	}
+
+	virtual const char* getModuleName() override {
+		return "AutoJump";
+	}
 };
