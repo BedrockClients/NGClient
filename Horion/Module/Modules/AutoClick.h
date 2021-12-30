f349edeb0544370ff7bf02949be72f954cb03712
DeadtrosGaming <isaiahmlam200@outlook.com>
2021-12-29 20:14:56 -0500

    .h

--- Horion/Module/Modules/AutoClick.h
+++ Horion/Module/Modules/AutoClick.h
@@ -1,9 +1,8 @@
 #pragma once
 #include "../ModuleManager.h"
 #include "Module.h"
-
 class AutoClick : public IModule {
-private:
+public:
 	int delay = 0;
 	int Odelay = 0;
 	bool weapons = true;
@@ -11,11 +10,63 @@ private:
 	bool rightclick = false;
 	bool hold = false;
 
-public:
-	AutoClick();
-	~AutoClick();
+	AutoClick() : IModule(0x0, Category::COMBAT, "A simple AutoClick, automatically clicks for you.") {
+		registerBoolSetting("rightclick", &rightclick, rightclick);
+		registerBoolSetting("only weapons", &weapons, weapons);
+		registerBoolSetting("break blocks", &breakBlocks, breakBlocks);
+		registerIntSetting("delay", &delay, delay, 0, 20);
+		registerBoolSetting("hold", &hold, hold);
+	 };
+	~AutoClick(){};
 
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
-	virtual void onTick(C_GameMode* gm) override;
+	void onTick(C_GameMode* gm) {
+		if ((GameData::isLeftClickDown() || !hold)) {
+			C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
+			PointingStruct* pointing = g_Data.getClientInstance()->getPointerStruct();
+			Odelay++;
+
+			if (Odelay >= delay) {
+				auto selectedItem = localPlayer->getSelectedItem();
+				if (weapons && selectedItem->getAttackingDamageWithEnchants() < 1)
+					return;
+				if (!rightclick) {
+					if (pointing->hasEntity()) {
+						g_Data.leftclickCount++;
+						localPlayer->swingArm();
+						gm->attack(pointing->getEntity());
+					} else if (breakBlocks) {
+						bool isDestroyed = false;
+						localPlayer->swingArm();
+						g_Data.leftclickCount++;
+						gm->startDestroyBlock(pointing->block, pointing->blockSide, isDestroyed);
+						if (isDestroyed && localPlayer->region->getBlock(pointing->block)->blockLegacy->blockId != 0)
+							gm->destroyBlock(&pointing->block, pointing->blockSide);
+						if (isDestroyed)
+							gm->stopDestroyBlock(pointing->block);
+					}
+					Odelay = 0;
+				}
+			}
+		}
+
+		if (rightclick) {
+			if ((GameData::isRightClickDown() || !hold) && g_Data.isInGame()) {
+				PointingStruct* pstruct = g_Data.getClientInstance()->getPointerStruct();
+				Odelay++;
+				if (Odelay >= delay) {
+					g_Data.rightclickCount++;
+					C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
+					localPlayer->swingArm();
+					gm->startBuildBlock(vec3_ti(pstruct->block), pstruct->blockSide);
+					gm->stopBuildBlock();
+					gm->buildBlock(new vec3_ti(pstruct->block), pstruct->blockSide);
+					Odelay = 0;
+				}
+			}
+		}
+	}
+
+	virtual const char* getModuleName() override {
+		return "AutoClick";
+	}
 };
