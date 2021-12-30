fd0e802ee90a355af9d58b89c0f6ed6f0817d854
YDC-Deadly <88569435+YDC-Deadly@users.noreply.github.com>
2021-12-29 21:30:18 -0500

    Revert ".h"
    This reverts commit fc0b0e0e312cc60b4de26849b3bfa81af7df4381.

--- Horion/Module/Modules/AutoJump.h
+++ Horion/Module/Modules/AutoJump.h
@@ -1,21 +1,12 @@
 #pragma once
-#include "../ModuleManager.h"
 #include "Module.h"
+
 class AutoJump : public IModule {
 public:
+	AutoJump();
+	~AutoJump();
 
-	AutoJump() : IModule(0x0, Category::MOVEMENT, "Automatically jump"){
-
-	};
-	~AutoJump(){};
-
-	void onTick(C_GameMode* gm) {
-		auto player = g_Data.getLocalPlayer();
-
-		if (player->onGround) gm->player->jumpFromGround();
-	}
-
-	virtual const char* getModuleName() override {
-		return "AutoJump";
-	}
+	// Inherited via IModule
+	virtual const char* getModuleName() override;
+	virtual void onTick(C_GameMode* gm) override;
 };
