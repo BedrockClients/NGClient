8bf4dfbab93894e055dbf62af6a9dbcc155aa546
YDC-Deadly <88569435+YDC-Deadly@users.noreply.github.com>
2021-12-31 18:15:35 -0500

    yes.h

--- Horion/Module/Modules/FullBright.h
+++ Horion/Module/Modules/FullBright.h
@@ -1,15 +1,40 @@
 #pragma once
+#include "../ModuleManager.h"
 #include "Module.h"
 class FullBright : public IModule {
 public:
 	float intensity = 25.f;
 	float* gammaPtr = nullptr;
-	FullBright();
-	~FullBright();
 
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
-	virtual void onTick(C_GameMode* gm) override;
-	virtual void onEnable() override;
-	virtual void onDisable() override;
+	FullBright() : IModule(0x0, Category::VISUAL, "Puts your gamma to max") {
+		registerFloatSetting("Intensity", &intensity, intensity, -25.f, 25.f);
+	};
+	~FullBright(){};
+
+	float originalGamma = -1;
+
+	void onTick(C_GameMode* gm) {
+		if (gammaPtr != nullptr && *gammaPtr != 10)
+			*gammaPtr = 10;
+	}
+
+	void onEnable() {
+		if (gammaPtr != nullptr) {
+			originalGamma = *gammaPtr;
+			*gammaPtr = 10;
+		}
+	}
+
+	void onDisable() {
+		if (gammaPtr != nullptr) {
+			if (originalGamma >= 0 && originalGamma <= 1)
+				*gammaPtr = originalGamma;
+			else
+				*gammaPtr = 0.5f;
+		}
+	}
+
+	virtual const char* getModuleName() override {
+		return "FullBright";
+	}
 };
