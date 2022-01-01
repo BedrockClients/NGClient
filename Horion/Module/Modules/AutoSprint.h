31d36592b0df72e6a0aaa0a182b96944526515d7
YDC-Deadly <88569435+YDC-Deadly@users.noreply.github.com>
2022-01-01 00:43:10 -0500

    .h

--- Horion/Module/Modules/AutoSprint.h
+++ Horion/Module/Modules/AutoSprint.h
@@ -1,13 +1,26 @@
 #pragma once
+#include "../ModuleManager.h"
 #include "Module.h"
 class AutoSprint : public IModule {
-private:
+public:
 	bool alldirections = true;
 
-public:
-	AutoSprint();
-	~AutoSprint();
+	AutoSprint() : IModule(0x0, Category::MOVEMENT, "Automatically sprint without holding the key") {
+		registerBoolSetting("all directions", &alldirections, alldirections);
+	};
+	~AutoSprint(){};
 
-	virtual void onTick(C_GameMode* gm) override;
-	virtual const char* getModuleName() override;
+	void onTick(C_GameMode* gm) {
+		C_GameSettingsInput* input = g_Data.getClientInstance()->getGameSettingsInput();
+		if (!GameData::isKeyDown(*input->sneakKey)) {
+			if (!gm->player->isSprinting() && gm->player->velocity.magnitudexz() > 0.01f) {
+				if (alldirections || GameData::isKeyDown(*input->forwardKey))
+					gm->player->setSprinting(true);
+			}
+		}
+	}
+
+	virtual const char* getModuleName() override {
+		return "AutoSprint";
+	}
 };
