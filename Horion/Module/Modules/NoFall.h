f2e7c587007e65f0fbc61cfc99f51e77e334ca50
YDC-Deadly <88569435+YDC-Deadly@users.noreply.github.com>
2021-12-31 23:32:33 -0500

    .h

--- Horion/Module/Modules/NoFall.h
+++ Horion/Module/Modules/NoFall.h
@@ -1,25 +1,57 @@
 #pragma once
 #include "../ModuleManager.h"
 #include "Module.h"
-
 class NoFall : public IModule {
-private:
+public:
 	bool groundy = false;
 	int counter = 1;
 	bool hive = true;
 	bool motion = false;
 	float glideMod = 0.f;
 	float glideModEffective = 0;
-
-public:
 	bool server = false;
 	bool nopackety = false;
 	float range = 3;
-	NoFall();
-	~NoFall();
 
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
-	void onTick(C_GameMode* gm);
-	void onDisable();
+	NoFall() : IModule(0x0, Category::SERVER, "Gives You Shit Loads Of XP!"){
+		registerBoolSetting("OnGround", &groundy, groundy);
+		registerBoolSetting("Server", &server, server);
+		registerBoolSetting("NoPacket", &nopackety, nopackety);
+		registerBoolSetting("Motion", &motion, motion);
+		registerBoolSetting("Hive", &hive, hive);	
+	};
+	~NoFall(){};
+
+	void onTick(C_GameMode* gm) {
+		C_LocalPlayer* player = g_Data.getLocalPlayer();
+		if (groundy && g_Data.getLocalPlayer() != nullptr && g_Data.getLocalPlayer()->fallDistance > 2.f) {
+			gm->player->onGround = true;
+		}
+		if (hive && gm->player->fallDistance > 4.f) {
+			vec3_t pos = *gm->player->getPos();
+			pos.y += 50;
+			gm->player->tryTeleportTo(pos, true, true, 1, 1);
+			gm->player->onGround = true;
+			gm->player->fallDistance = 0.f;
+		}
+		if (motion) {
+			if (player->fallDistance > 4) {
+				glideModEffective = glideMod;
+				gm->player->velocity.y = glideModEffective;
+				g_Data.getLocalPlayer()->setPos((*g_Data.getLocalPlayer()->getPos()).add(0, 0.25, 0));  //sets how much u need
+			}
+		}
+		if (counter == 5)
+			counter = 0;
+		else
+			counter++;
+
+		vec3_t blockBelow2 = g_Data.getLocalPlayer()->eyePos0;  // Block below the player
+		blockBelow2.y -= g_Data.getLocalPlayer()->height;
+		blockBelow2.y -= 1.5f;
+	}
+
+	virtual const char* getModuleName() override {
+		return "NoFall";
+	}
 };
