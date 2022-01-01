16901698af92a8f28072cfdefae5ac8cc6a3f0cf
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2022-01-01 14:06:28 -0500

    Deadly fuck yourself and dont do one .h at a time

--- Horion/Module/Modules/Blink.h
+++ Horion/Module/Modules/Blink.h
@@ -6,12 +6,15 @@ private:
 	std::vector<PlayerAuthInputPacket*> PlayerAuthInputPacketHolder;
 
 public:
-	Blink();
-	~Blink();
+	Blink() : IModule(0, Category::PLAYER, "Stops you from sending packets and then sends them in a bunch") {}
+	~Blink() {
+		getMovePlayerPacketHolder()->clear();
+		getPlayerAuthInputPacketHolder()->clear();
+	}
 
 	// Inherited via IModule
-	virtual void onTick(C_GameMode* gm) override;
-	virtual const char* getModuleName() override;
+	virtual void onTick(C_GameMode* gm) override { gm->player->fallDistance = 0.f; }
+	virtual const char* getModuleName() override { return ("Blink"); }
 
 	inline std::vector<C_MovePlayerPacket*>* getMovePlayerPacketHolder() { return &MovePlayerPacketHolder; };
 	inline std::vector<PlayerAuthInputPacket*>* getPlayerAuthInputPacketHolder() { return &PlayerAuthInputPacketHolder; };
