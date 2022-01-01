750091e06354bc6ce7abe4b67425958ce040d063
YDC-Deadly <88569435+YDC-Deadly@users.noreply.github.com>
2021-12-31 23:11:40 -0500

    .h

--- Horion/Module/Modules/NightMode.h
+++ Horion/Module/Modules/NightMode.h
@@ -1,15 +1,16 @@
 #pragma once
 #include "../ModuleManager.h"
 #include "Module.h"
-
 class NightMode : public IModule {
 public:
-	NightMode();
-	~NightMode();
-
 	float modifier = 0.5f;
 
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
-	virtual void onEnable() override;
+	NightMode() : IModule(0x0, Category::VISUAL, "Darkens the environment"){
+		
+	};
+	~NightMode(){};
+
+	virtual const char* getModuleName() override {
+		return "NightMode";
+	}
 };
