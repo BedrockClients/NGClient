7b35117d4979a50913b1861b6643a898a8d687e7
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2022-01-01 14:26:29 -0500

    hmmm .h

--- Horion/Module/Modules/Speed.h
+++ Horion/Module/Modules/Speed.h
@@ -7,12 +7,28 @@ private:
 	float speed = 1.6f;
 
 public:
-	Speed();
-	~Speed();
+	Speed() : IModule(VK_NUMPAD2, Category::MOVEMENT, "Speed up!") {
+		registerFloatSetting("speed", &speed, 1, 0.1f, 3.f);
+	}
+	~Speed(){};
 
 	// Inherited via IModule
-	virtual const char* getModuleName() override;
-	virtual void onTick(C_GameMode* gm) override;
-	virtual void onEnable() override;
-	virtual void onDisable() override;
+	virtual const char* getModuleName() override { return ("Speed"); }
+	virtual void onTick(C_GameMode* gm) override {
+		C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
+		float* speedAdr = reinterpret_cast<float*>(g_Data.getLocalPlayer()->getSpeed() + 0x84);
+		*speedAdr = speed;
+	}
+	virtual void onEnable() override {
+		if (g_Data.getLocalPlayer() == nullptr) {
+			setEnabled(false);
+			return;
+		} else {
+			origSpeed = *reinterpret_cast<float*>(g_Data.getLocalPlayer()->getSpeed() + 0x84);
+		}
+	}
+	virtual void onDisable() override {
+		if (g_Data.getLocalPlayer() != nullptr)
+			*reinterpret_cast<float*>(g_Data.getLocalPlayer()->getSpeed() + 0x84) = origSpeed;
+	}
 };
--- Horion/Module/Modules/Step.h
+++ Horion/Module/Modules/Step.h
@@ -5,11 +5,16 @@ private:
 	float height = 2.f;
 
 public:
-	Step();
-	~Step();
+	Step() : IModule(0, Category::MOVEMENT, "Increases your step height") {
+		registerFloatSetting("height", &height, height, 1.f, 2.f);
+	}
+	~Step(){}
 
 	// Inherited via IModule
-	virtual const char* getModuleName() override;
-	virtual void onTick(C_GameMode* gm) override;
-	virtual void onDisable() override;
+	virtual const char* getModuleName() override { return ("Step"); }
+	virtual void onTick(C_GameMode* gm) override { gm->player->stepHeight = height; }
+	virtual void onDisable() override {
+		if (g_Data.getLocalPlayer() != nullptr)
+			g_Data.getLocalPlayer()->stepHeight = 0.5625f;
+	}
 };
