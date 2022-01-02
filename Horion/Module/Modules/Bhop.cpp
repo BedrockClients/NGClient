8d16e1e129d2f6b18c47d4dbbae281588dda2b30
HackerHansen <58196022+HackerHansen@users.noreply.github.com>
2022-01-02 14:09:42 -0800

    Add ZoomHop
    Use 0.83 speed, idk why it doesn't work at a higher speed on this client. It works at 1 speed fine on my client

--- Horion/Module/Modules/Bhop.cpp
+++ Horion/Module/Modules/Bhop.cpp
@@ -1,13 +1,16 @@
 #include "Bhop.h"
+
 #include <stack>
 float speed = 0.50f;
 float height = 1.f;
 bool hive = false;
 int timer = 20;
+bool ZoomHop = false;
 Bhop::Bhop() : IModule(0, Category::MOVEMENT, "Hop around like a bunny!") {
 	registerBoolSetting("Hive", &hive, hive);
 	registerFloatSetting("Speed", &speed, speed, 0.10f, 1.00f);
 	registerFloatSetting("Height", &height, height, 0.01f, 2.00f);
+	registerBoolSetting("ZoomHop", &ZoomHop, ZoomHop);
 	registerIntSetting("Timer", &timer, timer, 20, 50);
 }
 
@@ -21,6 +24,8 @@ const char* Bhop::getModuleName() {
 void Bhop::onEnable() {
 }
 
+C_MoveInputHandler cachedInput;
+
 void Bhop::onTick(C_GameMode* gm) {
 	g_Data.getClientInstance()->minecraft->setTimerSpeed(static_cast<float>(timer));
 }
@@ -28,7 +33,10 @@ void Bhop::onTick(C_GameMode* gm) {
 void Bhop::onSendPacket(C_Packet* packet) {
 }
 
+int lastTick = 0;
+
 void Bhop::onMove(C_MoveInputHandler* input) {
+	cachedInput = *input;
 	yes = input;
 	auto player = g_Data.getLocalPlayer();
 	if (player == nullptr) return;
@@ -42,8 +50,30 @@ void Bhop::onMove(C_MoveInputHandler* input) {
 	vec2_t moveVec2d = {input->forwardMovement, -input->sideMovement};
 	bool pressed = moveVec2d.magnitude() > 0.01f;
 
-	if (player->onGround && pressed)
-		player->jumpFromGround();
+	if (pressed) {
+		player->setSprinting(true);
+		if (player->onGround) {
+			if (ZoomHop) {
+				C_PlayerActionPacket jmp;
+				jmp.entityRuntimeId = player->entityRuntimeId;
+				jmp.face = 0;
+				jmp.action = 8;
+				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&jmp);
+			} 
+			else {
+				player->jumpFromGround();
+			}
+		} 
+		else if (player->velocity.y > -0.35f) {
+			player->velocity.y -= 0.12f;
+		}
+		C_MovePlayerPacket mpp(player, *player->getPos());
+		mpp.onGround = player->onGround;
+		mpp.pitch += 0.5f;
+		mpp.yaw += 0.5f;
+		mpp.headYaw += 0.5f;
+		g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&mpp);
+	}
 
 	float calcYaw = (player->yaw + 90) * (PI / 180);
 	vec3_t moveVec;
@@ -51,7 +81,8 @@ void Bhop::onMove(C_MoveInputHandler* input) {
 	float s = sin(calcYaw);
 	moveVec2d = {moveVec2d.x * c - moveVec2d.y * s, moveVec2d.x * s + moveVec2d.y * c};
 	moveVec.x = moveVec2d.x * speed;
-	moveVec.y = player->velocity.y * height;
+	if (ZoomHop) moveVec.y = 0.3f;
+	else moveVec.y = player->velocity.y * height;
 	moveVec.z = moveVec2d.y * speed;
 
 	if (pressed && hive && player->onGround) player->lerpMotion(moveVec);
