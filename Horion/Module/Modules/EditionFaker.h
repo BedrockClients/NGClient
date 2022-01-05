16648f52c125b1cd1497ad0cd3055c89c7f289ac
YDC-Deadly <88569435+YDC-Deadly@users.noreply.github.com>
2022-01-05 18:00:49 -0500

    EditionFaker

--- Horion/Module/Modules/EditionFaker.h
+++ Horion/Module/Modules/EditionFaker.h
@@ -7,9 +7,22 @@ private:
 
 public:
 	EditionFaker() : IModule(0x0, Category::SERVER, "Unknown" /*, Android, iOS, macOS, FireOS, GearVR, HoloLens, Windows 10(PC), Windows, Dedicated, Orbis, NX */) {
-		registerIntSetting("Edition", &edition, edition, -1, 14);
-		//not know
-		//-1 11 12 13 14
+		registerIntSetting("Edition", &edition, edition, 1, 13);
+		/* 
+		1 Android
+		2 IOS
+		3
+		4 Kindle Fire
+		5
+		6
+		7 Wondows
+		8
+		9
+		10 PC
+		11 PS4
+		12 Switch
+		13 XBOX*/
+		
 	};
 	~EditionFaker(){};
 
