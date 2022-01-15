6c393ad3e754e23b7e211350d67b2946994321fa
HackerHansen <hackerhansengaming1672@gmail.com>
2022-01-14 20:10:40 -0800

    Go zoom on da Hive

--- Horion/Module/Modules/HiveFly.cpp
+++ Horion/Module/Modules/HiveFly.cpp
@@ -1,153 +1,78 @@
 #include "HiveFly.h"
 int counter = 0;
-HiveFly::HiveFly() : IModule('0', Category::SERVER, "Turn it on, Throw a pearl, then start flying!!") {
-	registerFloatSetting("Speed", &speed, speed, 0.01f, 0.50f);
-	registerFloatSetting("Timer", &timer, timer, 20.f, 100.f);
+HiveFly::HiveFly() : IModule('0', Category::SERVER, "Wheeeeeeeeeeeeee") {
+
 }
 
 HiveFly::~HiveFly() {
 }
 
 const char* HiveFly::getModuleName() {
-	return ("HivePearlFly");
+	return ("HiveFly");
 }
+
+float epicHiveFlySpeedArrayThingy[15] = {
+	0.810000,
+	0.615560,
+	0.583347,
+	0.554032,
+	0.527356,
+	0.503081,
+	0.480991,
+	0.460888,
+	0.442595,
+	0.425948,
+	0.410800,
+	0.397015,
+	0.384470,
+	0.373055,
+	0.362666
+};
+
+int flySpeedIndex = 0;
+
+bool dontGoDown = true;
 
 void HiveFly::onEnable() {
 	srand(time(NULL));
 	counter = 0;
-	C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
-	slot = supplies->selectedHotbarSlot;
-}
-
-void HiveFly::onTick(C_GameMode* gm) {
-	float rotChange = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 2.f)) + 1.f;
-	counter++;
-	if (counter<= 2)
-		findBlock();
-
-	if (counter <= 10) {
-		C_LocalPlayer* player = g_Data.getLocalPlayer();
-		C_MovePlayerPacket mpp(player, *player->getPos());
-		mpp.onGround = player->onGround;
-		mpp.pitch = 90.f - rotChange;
-		g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&mpp);
+	flySpeedIndex = 0;
+	dontGoDown = true;
+	C_LocalPlayer* player = g_Data.getLocalPlayer();
+	if (player != nullptr) {
+		vec3_t myPos = *player->getPos();
+		myPos.y += 2.1f;
+		player->setPos(myPos);
+		player->jumpFromGround();
 	}
-	C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
-	if(counter == 2)
-		Utils::rightClick();
-	if (counter == 3)
-		supplies->selectedHotbarSlot = slot;
-	gm->player->velocity.y = 0.f;
-	if (isEnabled())
-		*g_Data.getClientInstance()->minecraft->timer = timer;
-}
-
-bool HiveFly::findBlock() {
-	C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
-	C_Inventory* inv = supplies->inventory;
-	auto prevSlot = supplies->selectedHotbarSlot;
-	for (int n = 0; n < 36; n++) {
-		C_ItemStack* stack = inv->getItemStack(n);
-		if (stack->item != nullptr) {
-			if (stack->getItem()->itemId == 422) {
-				if (prevSlot != n)
-					supplies->selectedHotbarSlot = n;
-				return true;
-			}
-		}
-	}
-	return false;
 }
 
 void HiveFly::onMove(C_MoveInputHandler* input) {
-	if (counter >= 15) {
-		C_LocalPlayer* player = g_Data.getLocalPlayer();
-		if (player == nullptr) return;
+	C_LocalPlayer* player = g_Data.getLocalPlayer();
+	if (player == nullptr) return;
 
-		vec2_t moveVec2d = {input->forwardMovement, -input->sideMovement};
-		bool pressed = moveVec2d.magnitude() > 0.01f;
+	vec2_t moveVec2d = {input->forwardMovement, -input->sideMovement};
+	bool pressed = moveVec2d.magnitude() > 0.01f;
 
-		float calcYaw = (player->yaw + 90) * (PI / 180);
-		vec3_t moveVec;
-		float c = cos(calcYaw);
-		float s = sin(calcYaw);
-		moveVec2d = {moveVec2d.x * c - moveVec2d.y * s, moveVec2d.x * s + moveVec2d.y * c};
-		moveVec.x = moveVec2d.x * speed;
+	float calcYaw = (player->yaw + 90) * (PI / 180);
+	vec3_t moveVec;
+	float c = cos(calcYaw);
+	float s = sin(calcYaw);
+	moveVec2d = {moveVec2d.x * c - moveVec2d.y * s, moveVec2d.x * s + moveVec2d.y * c};
+	float moveSpeed = epicHiveFlySpeedArrayThingy[flySpeedIndex++ % 15];
+	moveVec.x = moveVec2d.x * moveSpeed;
+
+	if (dontGoDown)
 		moveVec.y = 0.f;
-		moveVec.z = moveVec2d.y * speed;
-		if (pressed) player->lerpMotion(moveVec);
-		if (!pressed) {
-			player->velocity.x = 0;
-			player->velocity.z = 0;
-		}
-		if (g_Data.canUseMoveKeys()) {
-			if (input->isJumping) {
-				player->velocity.y += speed;
-			}
-			if (input->isSneakDown) {
-				player->velocity.y -= speed;
-			}
-		}
+	else
+		moveVec.y = player->velocity.y;
+	dontGoDown = !dontGoDown;
 
-		if (player == nullptr)
-			return;
-
-		std::vector<vec3_ti> sideBlocks;
-		sideBlocks.reserve(8);
-		if (!pressed)
-			return;
-		moveVec2d = moveVec2d.normalized();
-		moveVec2d = {moveVec2d.x * c - moveVec2d.y * s, moveVec2d.x * s + moveVec2d.y * c};
-		for (int x = -1; x <= 1; x++) {
-			for (int z = -1; z <= 1; z++) {
-				if (x == 0 && z == 0)
-					continue;
-				if (moveVec2d.dot(vec2_t(x, z).normalized()) < 0.6f)
-					continue;
-				sideBlocks.push_back(vec3_ti(x, 0, z));
-			}
-		}
-		auto pPos = *player->getPos();
-		pPos.y = player->aabb.lower.y;
-		auto pPosI = vec3_ti(pPos.floor());
-		auto isObstructed = [&](int yOff, AABB* obstructingBlock = nullptr, bool ignoreYcoll = false) {
-			for (const auto& current : sideBlocks) {
-				vec3_ti side = pPosI.add(0, yOff, 0).add(current);
-				if (side.y < 0 || side.y >= 256)
-					break;
-				auto block = player->region->getBlock(side);
-				if (block == nullptr || block->blockLegacy == nullptr)
-					continue;
-				C_BlockLegacy* blockLegacy = block->toLegacy();
-				if (blockLegacy == nullptr)
-					continue;
-				AABB collisionVec;
-				if (!blockLegacy->getCollisionShape(&collisionVec, block, player->region, &side, player))
-					continue;
-				bool intersects = ignoreYcoll ? collisionVec.intersectsXZ(player->aabb.expandedXZ(0.1f)) : collisionVec.intersects(player->aabb.expandedXZ(0.1f));
-
-				if (intersects) {
-					if (obstructingBlock != nullptr)
-						*obstructingBlock = collisionVec;
-					return true;
-				}
-			}
-			return false;
-		};
-
-		AABB lowerObsVec, upperObsVec;
-		bool upperObstructed = isObstructed(1, &upperObsVec);
-
-		bool lowerObstructed = isObstructed(0, &lowerObsVec);
-		float targetSpeed = speed;
-		//actually moving code
-		if (upperObstructed || lowerObstructed) {
-			setEnabled(false);
-		}
-	} else {
-		g_Data.getLocalPlayer()->velocity.x = 0;
-		g_Data.getLocalPlayer()->velocity.y = 0;
-		g_Data.getLocalPlayer()->velocity.z = 0;
+	moveVec.z = moveVec2d.y * moveSpeed;
+	if (pressed) player->lerpMotion(moveVec);
+	if (!pressed) {
+		player->velocity.x = 0;
+		player->velocity.z = 0;
 	}
 }
 
--- Horion/Module/Modules/HiveFly.h
+++ Horion/Module/Modules/HiveFly.h
@@ -3,16 +3,11 @@
 class HiveFly : public IModule {
 private:
 public:
-	int slot;
-	float timer = 20.f;
-	float speed = 1.f;
-	bool findBlock();
 	HiveFly();
 	~HiveFly();
 
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
-	virtual void onTick(C_GameMode* gm) override;
 	virtual void onMove(C_MoveInputHandler* input) override;
 	virtual void onEnable() override;
 	virtual void onDisable() override;
