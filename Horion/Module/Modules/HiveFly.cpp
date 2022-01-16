702e48476043aec5a5b3bbf9df4bf6da0b440051
NRG <jcazman19@yahoo.com>
2022-01-15 23:37:38 -0500

    BLACK PEOPLE

--- Horion/Module/Modules/HiveFly.cpp
+++ Horion/Module/Modules/HiveFly.cpp
@@ -55,7 +55,7 @@ void HiveFly::onEnable() {
 				if (counter69 <= 2) {
 					vec3_t moveVec;
 					moveVec.x = 0;
-					moveVec.y = 0.8f;
+					moveVec.y = 1.f;
 					moveVec.z = 0;
 					g_Data.getLocalPlayer()->lerpMotion(moveVec);
 				}
