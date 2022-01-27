5a16316c674a947ff3100c86dba8145247c4efd4
YDC-Deadly <88569435+YDC-Deadly@users.noreply.github.com>
2022-01-27 08:46:38 -0500

    .h yes

--- Horion/Module/Modules/InstaBreak.h
+++ Horion/Module/Modules/InstaBreak.h
@@ -1,12 +1,33 @@
 #pragma once
+#include "../ModuleManager.h"
 #include "Module.h"
-
 class InstaBreak : public IModule {
 public:
 	SettingEnum mode;
-	InstaBreak();
-	~InstaBreak();
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
-	virtual void onTick(C_GameMode* gm) override;
+
+	InstaBreak() : IModule(0x0, Category::WORLD, "Break any block instantly") {
+		registerEnumSetting("Mode", &mode, 0);
+		mode = SettingEnum(this)
+				   .addEntry(EnumEntry("Normal", 0))
+				   .addEntry(EnumEntry("Hold", 1))
+				   .addEntry(EnumEntry("FastBreak", 2));
+	};
+	~InstaBreak(){};
+
+	void onTick(C_GameMode* gm) {
+		if (GameData::isLeftClickDown() && g_Data.isInGame() && g_Data.canUseMoveKeys() && g_Data.getLocalPlayer()->canOpenContainerScreen() && mode.selected == 1) {
+			PointingStruct* pointing = g_Data.getClientInstance()->getPointerStruct();
+			bool isDestroyed = false;
+			gm->stopDestroyBlock(pointing->block);
+			gm->startDestroyBlock(pointing->block, pointing->blockSide, isDestroyed);
+			vec3_ti* thingy = new vec3_ti(pointing->block);
+			gm->destroyBlock(thingy, pointing->blockSide);
+			gm->stopDestroyBlock(pointing->block);
+			delete thingy;
+		}
+	}
+
+	virtual const char* getModuleName() override {
+		return "InstaBreak";
+	}
 };
