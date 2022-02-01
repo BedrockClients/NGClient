b8ec712d20f7d0436e1ef061fc93f369cf393ff2
YDC-Deadly <88569435+YDC-Deadly@users.noreply.github.com>
2022-01-31 19:43:43 -0500

    tyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkbtyhjftgvghfcvgvhjfgvjftgyjgyjftgyujftgvjfgvjgvjgjccgjcgjctgyjcvyjikfvykjfvyjgjgjvghvhjghjbjkbkhbkjbjkb

--- Horion/Command/Commands/CoordsCommand.h
+++ Horion/Command/Commands/CoordsCommand.h
@@ -1,10 +1,18 @@
 #pragma once
+
 #include "ICommand.h"
+
 class CoordsCommand : public IMCCommand {
 public:
-	CoordsCommand();
-	~CoordsCommand();
+	CoordsCommand() : IMCCommand("coords", "Prints your coordinates", "X, Y, Z") {
+		registerAlias("pos");
+	}
+	~CoordsCommand(){};
 
-	// Inherited via IMCCommand
-	virtual bool execute(std::vector<std::string>* args) override;
+	bool execute(std::vector<std::string>* args) {
+		vec3_t* pos = g_Data.getLocalPlayer()->getPos();
+		float yPos = pos->y - 1.62f;
+		clientMessageF("[%sNG%s] %sX: %.2f Y: %.2f Z: %.2f", GOLD, WHITE, LIGHT_PURPLE, pos->x, yPos /* eye height */, pos->z);
+		return true;
+	}
 };
--- Horion/Module/Modules/EditionFaker.h
+++ Horion/Module/Modules/EditionFaker.h
@@ -4,20 +4,26 @@
 class EditionFaker : public IModule {
 private:
 	int edition = 0;
+	SettingEnum Edition;
 
 public:
 	EditionFaker() : IModule(0x0, Category::SERVER, "Unknown" /*, Android, iOS, macOS, FireOS, GearVR, HoloLens, Windows 10(PC), Windows, Dedicated, Orbis, NX */) {
-		registerIntSetting("Edition", &edition, edition, 1, 13);
+		//registerIntSetting("Edition", &edition, edition, 1, 13);
+		registerEnumSetting("Edition", &Edition, 13);
+		Edition = SettingEnum(this)
+					  .addEntry(EnumEntry("Android", 1))
+					  .addEntry(EnumEntry("IOS", 2))
+					  .addEntry(EnumEntry("Kindle Fire", 4))
+					  .addEntry(EnumEntry("Wondows", 7))
+					  .addEntry(EnumEntry("PS4", 11))
+					  .addEntry(EnumEntry("Switch", 12))
+					  .addEntry(EnumEntry("XBOX", 13))
+		;
 		/* 
 		1 Android
 		2 IOS
-		3
 		4 Kindle Fire
-		5
-		6
 		7 Wondows
-		8
-		9
 		10 PC
 		11 PS4
 		12 Switch
