d3f9ed9788c4b825a69e00c0387aa48aafb58b9b
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2022-03-07 16:44:31 -0500

    Update Hooks.cpp

--- Memory/Hooks.cpp
+++ Memory/Hooks.cpp
@@ -591,6 +591,10 @@ __int64 Hooks::RenderText(__int64 a1, C_MinecraftUIRenderContext* renderCtx) {
 			text.y += 10.f;
 			outline.y += 10.f;
 			DrawUtils::drawText(text, &string, wight, 1.f, 1.f);
+			string = "[*] Many Fixes";
+			text.y += 10.f;
+			outline.y += 10.f;
+			DrawUtils::drawText(text, &string, wight, 1.f, 1.f);
 			text.y += 15.f;
 			outline.y += 15.f;
 			string = "Credits:";
