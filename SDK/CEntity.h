8c08093c358df6bb1355d7113a27433a62a6c750
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2022-03-07 18:23:10 -0500

    i am bored so why not

--- SDK/CEntity.h
+++ SDK/CEntity.h
@@ -625,6 +625,12 @@ public:
 		*(int *)((uintptr_t)(this) + 0x73C) = 10;
 		*(int *)((uintptr_t)(this) + 0x740) = 0;
 	}
+
+	void spawnDustParticles(int intensity) {
+		using spawnDustParticles = void(__fastcall *)(C_Entity*, int);
+		static spawnDustParticles spawnParticle = reinterpret_cast<spawnDustParticles>(Utils::FindSignature("48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 56 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 8B EA 48 8B F1"));
+		spawnParticle(this, intensity);
+	}
 };
 #pragma pack(pop)
 
