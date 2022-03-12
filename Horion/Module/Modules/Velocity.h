2413013d08ed56d1d43abce962feaef008208457
YDC-Deadly <mfsjdeadly@gmail.com>
2022-03-12 15:55:15 -0500

    move

--- Horion/Module/Modules/Velocity.h
+++ Horion/Module/Modules/Velocity.h
@@ -6,7 +6,7 @@ public:
 	float xModifier = 0.f;
 	float yModifier = 0.f;
 
-	Velocity() : IModule(0x0, Category::VISUAL, "Replaces Sounds") {
+	Velocity() : IModule(0x0, Category::MOVEMENT, "Replaces Sounds") {
 		registerFloatSetting("Linear Modifier", &xModifier, xModifier, 0.f, 1.f);
 		registerFloatSetting("Height Modifier", &yModifier, yModifier, 0.f, 1.f);
 };
