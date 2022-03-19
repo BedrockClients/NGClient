6857bde1b549ef99f9a3f2a905c88a02cbefa74c
YDC-Deadly <mfsjdeadly@gmail.com>
2022-03-19 11:58:45 -0400

    pp

--- Horion/Module/Modules/Jesus.h
+++ Horion/Module/Modules/Jesus.h
@@ -1,14 +1,35 @@
 #pragma once
+#include "../ModuleManager.h"
 #include "Module.h"
 class Jesus : public IModule {
-private:
-	bool walk = false;
+public:
 	bool wasInWater = false;
 
-public:
-	Jesus();
-	~Jesus();
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
-	virtual void onTick(C_GameMode* gm) override;
+	Jesus() : IModule(0x0, Category::MOVEMENT, "Walk over water, like Jesus") {
+	};
+	~Jesus(){};
+
+	void Jesus::onTick(C_GameMode* gm) {
+		if (gm->player->isSneaking()) return;
+
+		if (gm->player->hasEnteredWater()) {
+			gm->player->velocity.y = 0.06f;
+			gm->player->onGround = true;
+			wasInWater = true;
+		} else if (gm->player->isInWater() || gm->player->isInLava()) {
+			gm->player->velocity.y = 0.1f;
+			gm->player->onGround = true;
+			wasInWater = true;
+		} else {
+			if (wasInWater) {
+				wasInWater = false;
+				gm->player->velocity.x *= 1.2f;
+				gm->player->velocity.x *= 1.2f;
+			}
+		}
+	}
+
+	virtual const char* getModuleName() override {
+		return "Jesus";
+	}
 };
