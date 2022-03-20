2084a23f0fec80d9f0db073b6d93debf1ac1eb78
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2022-03-20 18:20:12 -0400

    d

--- Horion/Module/Modules/InfiniteAura.cpp
+++ Horion/Module/Modules/InfiniteAura.cpp
@@ -36,7 +36,6 @@ void findEntities(C_Entity* currentEntity, bool isRegularEntitie) {
 	if (currentEntity->timeSinceDeath > 0 || currentEntity->damageTime >= 7) return;
 	if (FriendList::findPlayer(currentEntity->getNameTag()->getText())) return;//Skip Friend
 	if (InfiniteAuraMod->isMobAura) {
-	std::sort(targetList0.begin(), targetList0.end(), CompareTargetEnArray());
 	if (currentEntity->getNameTag()->getTextLength() <= 1 && currentEntity->getEntityTypeId() == 63) return;
 	if (currentEntity->width <= 0.01f || currentEntity->height <= 0.01f) return;//Don't hit this pesky antibot on 2b2e.org
 	if (currentEntity->getEntityTypeId() == 64) return;//item
@@ -58,81 +57,81 @@ void InfiniteAura::onEnable() {
 
 void InfiniteAura::onTick(C_GameMode* gm) {
 	if (g_Data.getClientInstance()->isInGame()) {
-		targetListC = targetList0.empty();
-		targetList0.clear();
-		std::sort(targetList0.begin(), targetList0.end(), CompareTargetEnArray());
 		targetList0.clear();
 		g_Data.forEachEntity(findEntities);
-		std::sort(targetList0.begin(), targetList0.end(), CompareTargetEnArray());
-		Odelay++;
 
-		if (targetList0.size() > 0 && Odelay >= delay) {
+		if (!targetList0.empty()) {
+			targetListC = targetList0.empty();
 			std::sort(targetList0.begin(), targetList0.end(), CompareTargetEnArray());
-			g_Data.getLocalPlayer()->swingArm();
+			Odelay++;
 
-			float calcYaw = (gm->player->yaw + 90) * (PI / 180);
-			float calcPitch = (gm->player->pitch) * -(PI / 180);
+			if (targetList0.size() > 0 && Odelay >= delay) {
+				g_Data.getLocalPlayer()->swingArm();
 
-			float teleportX = cos(calcYaw) * cos(calcPitch) * 3.5f;
-			float teleportZ = sin(calcYaw) * cos(calcPitch) * 3.5f;
-			C_MovePlayerPacket teleportPacket;
+				float calcYaw = (gm->player->yaw + 90) * (PI / 180);
+				float calcPitch = (gm->player->pitch) * -(PI / 180);
 
-			// Attack all entitys in targetList
-			if (hivee) {
-				if (isMulti) {
-					for (int i = 0; i < targetList0.size(); i++) {
-						vec3_t pos = *targetList0[i]->getPos();
-						teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), vec3_t(pos.x - teleportX, pos.y, pos.z - teleportZ));
+				float teleportX = cos(calcYaw) * cos(calcPitch) * 3.5f;
+				float teleportZ = sin(calcYaw) * cos(calcPitch) * 3.5f;
+				C_MovePlayerPacket teleportPacket;
+
+				// Attack all entitys in targetList
+				if (hivee) {
+					if (isMulti) {
+						for (int i = 0; i < targetList0.size(); i++) {
+							vec3_t pos = *targetList0[i]->getPos();
+							teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), vec3_t(pos.x - teleportX, pos.y, pos.z - teleportZ));
+							g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
+							g_Data.getCGameMode()->attack(targetList0[i]);
+						}
+					} else {
+						vec3_t pos = *targetList0[0]->getPos();
+						teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), vec3_t(pos.x, pos.y, pos.z));
 						g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
-						g_Data.getCGameMode()->attack(targetList0[i]);
+						g_Data.getCGameMode()->attack(targetList0[0]);
 					}
 				} else {
-					vec3_t pos = *targetList0[0]->getPos();
-					teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), vec3_t(pos.x, pos.y, pos.z));
-					g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
-					g_Data.getCGameMode()->attack(targetList0[0]);
-				}
-			} else {
-				if (strcmp(g_Data.getRakNetInstance()->serverIp.getText(), "mco.cubecraft.net") == 0) {
-					vec3_t pos = *g_Data.getLocalPlayer()->getPos();
+					if (strcmp(g_Data.getRakNetInstance()->serverIp.getText(), "mco.cubecraft.net") == 0) {
+						vec3_t pos = *g_Data.getLocalPlayer()->getPos();
 
-					C_MovePlayerPacket movePlayerPacket(g_Data.getLocalPlayer(), pos);
-					g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&movePlayerPacket);
+						C_MovePlayerPacket movePlayerPacket(g_Data.getLocalPlayer(), pos);
+						g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&movePlayerPacket);
 
-					pos.y += 0.35f;
+						pos.y += 0.35f;
 
-					movePlayerPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), pos);
-					g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&movePlayerPacket);
-				}
-				if (isMulti) {
-					for (int i = 0; i < targetList0.size(); i++) {
-						vec3_t pos = *targetList0[i]->getPos();
+						movePlayerPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), pos);
+						g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&movePlayerPacket);
+					}
+					if (isMulti) {
+						for (int i = 0; i < targetList0.size(); i++) {
+							vec3_t pos = *targetList0[i]->getPos();
+							teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), vec3_t(pos.x - teleportX, pos.y, pos.z - teleportZ));
+							g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
+							g_Data.getCGameMode()->attack(targetList0[i]);
+							teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos());
+							g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
+						}
+					} else {
+						vec3_t pos = *targetList0[0]->getPos();
 						teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), vec3_t(pos.x - teleportX, pos.y, pos.z - teleportZ));
 						g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
-						g_Data.getCGameMode()->attack(targetList0[i]);
+						g_Data.getCGameMode()->attack(targetList0[0]);
 						teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos());
 						g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
 					}
-				} else {
-					vec3_t pos = *targetList0[0]->getPos();
-					teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), vec3_t(pos.x - teleportX, pos.y, pos.z - teleportZ));
-					g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
-					g_Data.getCGameMode()->attack(targetList0[0]);
-					teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos());
-					g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
 				}
+				amogus = g_Data.getLocalPlayer()->getPos()->CalcAngle(*targetList0[0]->getPos());
+				Odelay = 0;
 			}
-			amogus = g_Data.getLocalPlayer()->getPos()->CalcAngle(*targetList0[0]->getPos());
-			Odelay = 0;
 		}
+		if (counter == 30) {
+			counter = 0;
+		} else {
+			counter++;
+		}
+		if (!g_Data.isInGame())
+			setEnabled(false);
 	}
-	if (counter == 30) {
-		counter = 0;
-	} else {
-		counter++;
-	}
-	if (!g_Data.isInGame())
-		setEnabled(false);
 }
 
 void InfiniteAura::onSendPacket(C_Packet* packet) {  //this is big brain time
